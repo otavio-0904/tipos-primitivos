373572fcb38553799f194a647cdff0d83a172953
otavio-0904 <otavio.augusto.conceicao@escola.pr.gov.br>
2026-04-15 21:53:09 -0300

    Add files via upload

---
+++ tipos.js
@@ -0,0 +1,32 @@
+//boolean
+
+let ativo = true;
+
+console.log(typeof ativo);
+console.log(ativo);
+
+//number
+let idade = 25;
+let pi = 3.14;
+console.log(typeof idade);
+console.log(typeof pi);
+
+//string e template literals
+let nome = "Maria";
+let mensagem = `Olá, ${nome}!`;
+console.log(mensagem);
+
+//undefined
+let valorIndefinido;
+console.log(typeof valorIndefinido);
+console.log(valorIndefinido);
+
+//null
+let valorNulo = null;
+console.log(typeof valorNulo);
+console.log(valorNulo);
+
+//bigint
+let numeroGrande = 12121521541215465n;
+console.log(typeof numeroGrande);
+console.log(numeroGrande);
